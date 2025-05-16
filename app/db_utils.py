6cd62130f0a1dd93a502f054329ffd97e5e656b6
yasinmusani <musaniyasin@gmail.com>
2025-05-16 11:47:44 +0530

    Update db_utils.py

---
+++ app/db_utils.py
@@ -0,0 +1,12 @@
+import oracledb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return oracledb.connect(
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        dsn=os.getenv("DB_DSN"),
+    )
